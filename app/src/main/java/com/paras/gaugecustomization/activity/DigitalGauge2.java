1bfbefdc5c0e81ebb45d3407f35a0c09ff569894
Shishir Dwivedi <shishirdwivedi5@gmail.com>
2018-05-16 21:03:07 +0530

    added fuel meter using seekbar without needle

---
+++ app/src/main/java/com/paras/gaugecustomization/activity/DigitalGauge2.java
@@ -0,0 +1,28 @@
+package com.paras.gaugecustomization.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.TypedValue;
+
+import com.paras.gaugecustomization.R;
+
+public class DigitalGauge2 extends Activity {
+
+@Override
+protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gauge2);
+        init();
+        }
+
+private void init() {
+
+        }
+
+public int dpToPixels(int i) {
+        return (int) TypedValue
+        .applyDimension(TypedValue.COMPLEX_UNIT_DIP, i, getResources().getDisplayMetrics());
+
+        }
+        }
